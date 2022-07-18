45587ea75b0997aa41169633230a88df05ef33c8
mikemickey26 <107206773+mikemickey26@users.noreply.github.com>
2022-07-18 14:08:20 -0700

    upadted

---
+++ app.js
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <script type="text/javascript">
+            <!--
+            var x=2
+            console.log("Entering the loop<br /> ");
+            while (x < 30) {
+                if (x == 10){
+                    break; // breaks out of loop completely
+            }
+            {
+                x = x + 5;
+                console.log( x + "<br />");
+            }
+            console.log("Exiting the loop!<br /> ");
+            //-->
+        </script>
+    </body>
+</html>
